0bc497cbdd77ae5a715536238fa2ff6a723e5465
Victor Agababov <vagababov@gmail.com>
2014-08-24 20:17:36 -0700

    more functions, comments. add tests for positive, negative tests

--- go/intervalmath/intervalmath.go
+++ go/intervalmath/intervalmath.go
@@ -15,11 +15,17 @@ type Interval struct {
 	e float64
 }
 
+// New returns a new Interval.
+// error is returned if e < s.
 func New(s, e float64) (*Interval, error) {
 	if s > e {
 		return nil, fmt.Errorf("start(%g) > end(%g)", s, e)
 	}
 	return &Interval{s, e}, nil
+}
+
+func (i *Interval) String() string {
+	return fmt.Sprintf("[%g, %g]", i.s, i.e)
 }
 
 // Positive returns if interval is completely positive.
@@ -54,15 +60,33 @@ func (i *Interval) ApproximatelyEquals(o *Interval, precion float64) bool {
 	return math.Abs(i.e-o.e) <= precion && math.Abs(i.s-o.s) <= precion
 }
 
+// ContainsZero returns true if interval contains 0.
 func (i *Interval) ContainsZero() bool {
 	return i.s <= 0 && i.e >= 0
 }
 
-func (i *Interval) AddTo(o *Interval) {
-	i.s += o.s
-	i.e += o.e
-}
-
+// Add returns the sum of a and b.
 func Add(a, b *Interval) *Interval {
 	return &Interval{a.s + b.s, a.e + b.e}
+}
+
+// Sub returns difference between a and b.
+func Sub(a, b *Interval) *Interval {
+	return &Interval{a.s - b.e, a.e - b.s}
+}
+
+// Inverse returns the inverse of the interval if it does not contain zero, nil
+// will be returned for those intervals.
+// For intervals containing zero use two values InverseEx function.
+func Inverse(a *Interval) *Interval {
+	if a.ContainsZero() {
+		return nil
+	}
+	return &Interval{1. / a.e, 1 / a.s}
+}
+
+// InverseEx returns (-inf, 1/a.s] and [1/a.e, +inf). This can be used to
+// compute division by interval that contains zero.
+func InverseEx(a *Interval) (l, r *Interval) {
+	return &Interval{math.Inf(-1), 1. / a.s}, &Interval{1. / a.e, math.Inf(1)}
 }
--- go/intervalmath/intervalmath_test.go
+++ go/intervalmath/intervalmath_test.go
@@ -10,10 +10,11 @@ func TestCreationAndProps(t *testing.T) {
 		wantContains0 bool
 		wantPositive  bool
 		wantNegative  bool
+		inv           *Interval
 	}{
-		{"simple", 1.0, 2.0, false, false, true, false},
-		{"with zero", -1.0, 1.0, false, true, false, false},
-		{"negative", -2, -1, false, false, false, true},
+		{"simple", 1.0, 2.0, false, false, true, false, &Interval{0.5, 1.0}},
+		{"with zero", -1.0, 1.0, false, true, false, false, nil},
+		{"negative", -2, -1, false, false, false, true, &Interval{-1., -0.5}},
 		{desc: "bad", s: -1, e: -2, wantErr: true},
 	}
 
@@ -28,7 +29,13 @@ func TestCreationAndProps(t *testing.T) {
 			continue
 		}
 		if got, want := i.ContainsZero(), test.wantContains0; got != want {
-			t.Errorf("i.ContainsZero: got: %v want: %v", got, want)
+			t.Errorf("%v.ContainsZero: got: %v want: %v", i, got, want)
+		}
+		if got, want := i.Positive(), test.wantPositive; got != want {
+			t.Errorf("%v.Positive: got: %v want: %v", i, got, want)
+		}
+		if got, want := i.Negative(), test.wantNegative; got != want {
+			t.Errorf("%v.Negative: got: %v want: %v", i, got, want)
 		}
 	}
 }
